0ef797518eca811289e68d1cbb856fdb3fdf4c96
Eirik Vikanes <Eirik@dhcp-083107.wlan.ntnu.no>
2015-10-16 13:43:13 +0200

    Fixed setting the wrong bit in CMU_HFPERCLKEN0

--- timer.c
+++ timer.c
@@ -4,15 +4,11 @@
 #include "efm32gg.h"
 
 /* function to setup the timer */
-void setupTimer(uint16_t period)
-{
-  /*
-    This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
-  */  
-  
+void setupTimer(uint16_t period) {
+  //  This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked. 
+
   //  1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0
-  uint16_t timer_bit = 0x06;
-  *CMU_HFPERCLKEN0 |= timer_bit;
+  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
   
   //  2. Write the period to register TIMER1_TOP
   *TIMER1_TOP = period;
@@ -22,7 +18,6 @@ void setupTimer(uint16_t period)
   
   //  4. Start the timer by writing 1 to TIMER1_CMD
   *TIMER1_CMD = 0x01;
-  
 }
 
 
